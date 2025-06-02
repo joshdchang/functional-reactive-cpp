ccb200b97597932222d78c39413826a8d9ae3c91
joshdchang <joshchang04@gmail.com>
2025-06-02 15:29:59 +1200

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello from functional-reactive-cpp!" << std::endl;
+    std::cout << "Hello from functional-reactive!" << std::endl;
     return 0;
 }
